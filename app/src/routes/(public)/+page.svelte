e7a44ba0f128e8b829ac1a4898f7a8902f75c1f8
Gary Mills <gmillswebdev@gmail.com>
2026-04-11 14:55:44 +0100

    tested, reduced base page back to minimum

--- app/src/routes/(public)/+page.svelte
+++ app/src/routes/(public)/+page.svelte
@@ -1,10 +1,7 @@
 <script>
-    import Modal from "$lib/components/ui/Modal.svelte";
+    
 </script>
 
 <h1>Welcome</h1>
 <p>public page</p>
-<Modal title="Hello" open={true} onclose={() => console.log('Modal closed')}>
-  <p>This is a modal dialog.</p>
-<p> another para</p>
-</Modal>
+
