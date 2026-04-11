80f95b2768efcc4542bcf9e3dd2e65c4cf704e01
Gary Mills <gmillswebdev@gmail.com>
2026-04-11 14:31:30 +0100

    automated hover colours for buttons

--- app/src/routes/(public)/+page.svelte
+++ app/src/routes/(public)/+page.svelte
@@ -1,7 +1,11 @@
 <script>
     import Input from "$lib/components/ui/Input.svelte";
+    import Button from "$lib/components/ui/Button.svelte";
 </script>
 
 <h1>Welcome</h1>
 <p>public page</p>
 <Input label="Email" name="email" type="email" placeholder="placeholder" required />        
+<Button variant="primary" size="md">
+    Submit
+</Button>
